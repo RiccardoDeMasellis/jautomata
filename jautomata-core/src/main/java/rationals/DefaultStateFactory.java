10602c6e9dbed571d39bc53b3aacac9f2ec3ef1e
Riccardo De Masellis <r.demasellis@trentorise.eu>
2015-05-08 17:50:43 +0200

    DefaultStateFactory modified for my needs

--- jautomata-core/src/main/java/rationals/DefaultStateFactory.java
+++ jautomata-core/src/main/java/rationals/DefaultStateFactory.java
@@ -16,7 +16,12 @@
  */
 package rationals;
 
-import java.util.HashSet;
+import java.lang.reflect.Array;
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
@@ -25,13 +30,10 @@ import java.util.Set;
  * createState
  *
  * @author Arnaud.Bailly - bailly@lifl.fr
+ * @version Thu Apr 25 2002
  */
-public class DefaultStateFactory<L, Tr extends Transition<L>, T extends Builder<L, Tr, T>> implements StateFactory<L, Tr, T>, Cloneable {
+public class DefaultStateFactory implements StateFactory, Cloneable {
 
-    private int id = 0;
-
-    private Automaton<L, Tr, T> automaton;
-	
     public class DefaultState implements State {
 
         public final int i;
@@ -40,7 +42,7 @@ public class DefaultStateFactory<L, Tr extends Transition<L>, T extends Builder<
 
         boolean terminal;
 
-        Automaton<L, Tr, T> a;
+        Automaton a;
 
         private Object label = null;
 
@@ -49,6 +51,24 @@ public class DefaultStateFactory<L, Tr extends Transition<L>, T extends Builder<
             this.a = automaton;
             this.initial = initial;
             this.terminal = terminal;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see salvo.jesus.graph.Vertex#getObject()
+         */
+        public Object getObject() {
+            return new Integer(i);
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see salvo.jesus.graph.Vertex#setObject(java.lang.Object)
+         */
+        public void setObject(Object object) {
+      /* NOOP */
         }
 
         /*
@@ -97,26 +117,276 @@ public class DefaultStateFactory<L, Tr extends Transition<L>, T extends Builder<
             return this.terminal;
         }
 
-        @Override
         public String toString() {
             return label == null ? Integer.toString(i) : label.toString();
         }
 
-        @Override
         public boolean equals(Object o) {
-            if (!(o instanceof DefaultStateFactory.DefaultState)) return false;
-            @SuppressWarnings("unchecked")
-			DefaultState ds = (DefaultState) o;
-            return (ds.i == i) && (a == ds.a);
+            try {
+                DefaultState ds = (DefaultState) o;
+                return (ds.i == i) && (a == ds.a);
+            } catch (ClassCastException e) {
+                return false;
+            }
         }
 
-        @Override
         public int hashCode() {
             return i;
         }
     }
-    
-    DefaultStateFactory(Automaton<L, Tr, T> a) {
+
+    class DefaultStateSet implements Set {
+
+        private DefaultStateFactory df;
+
+        /**
+         * @param set
+         */
+        public DefaultStateSet(DefaultStateSet set, DefaultStateFactory df) {
+            this.bits = (BitSet) set.bits.clone();
+            this.df = df;
+        }
+
+        /**
+         *
+         */
+        public DefaultStateSet(DefaultStateFactory df) {
+            this.df = df;
+        }
+
+        public boolean equals(Object obj) {
+            DefaultStateSet dss = (DefaultStateSet) obj;
+            return (dss == null) ? false : (dss.bits.equals(bits) && dss.df == df);
+        }
+
+        public int hashCode() {
+            return bits.hashCode();
+        }
+
+        public String toString() {
+            StringBuffer sb = new StringBuffer();
+            sb.append('[');
+            String b = bits.toString();
+            sb.append(b.substring(1, b.length() - 1));
+            sb.append(']');
+            return sb.toString();
+        }
+
+        int modcount = 0;
+
+        int mods = 0;
+
+        int bit = -1;
+
+        BitSet bits = new BitSet();
+
+        Iterator it = new Iterator() {
+
+            public void remove() {
+                if (bit > 0)
+                    bits.clear(bit);
+            }
+
+            public boolean hasNext() {
+                return bits.nextSetBit(bit) > -1;
+            }
+
+            public Object next() {
+                bit = bits.nextSetBit(bit);
+                if (bit == -1)
+                    throw new NoSuchElementException();
+                DefaultState ds = new DefaultState(bit, false, false);
+                ds.initial = automaton.initials().contains(ds);
+                ds.terminal = automaton.terminals().contains(ds);
+                mods++;
+                modcount++;
+                if (mods != modcount)
+                    throw new ConcurrentModificationException();
+        /* advance iterator */
+                bit++;
+                return ds;
+            }
+        };
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#size()
+         */
+        public int size() {
+            return bits.cardinality();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#clear()
+         */
+        public void clear() {
+            modcount++;
+            bits.clear();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#isEmpty()
+         */
+        public boolean isEmpty() {
+            return bits.isEmpty();
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#toArray()
+         */
+        public Object[] toArray() {
+            Object[] ret = new Object[size()];
+            Iterator it = iterator();
+            int i = 0;
+            while (it.hasNext()) {
+                ret[i++] = it.next();
+            }
+            return ret;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#add(java.lang.Object)
+         */
+        public boolean add(Object o) {
+            DefaultState ds = (DefaultState) o;
+            if (bits.get(ds.i))
+                return false;
+            bits.set(ds.i);
+            modcount++;
+            return true;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#contains(java.lang.Object)
+         */
+        public boolean contains(Object o) {
+            DefaultState ds = (DefaultState) o;
+            return bits.get(ds.i);
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#remove(java.lang.Object)
+         */
+        public boolean remove(Object o) {
+            DefaultState ds = (DefaultState) o;
+            if (!bits.get(ds.i))
+                return false;
+            bits.clear(ds.i);
+            modcount++;
+            return true;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#addAll(java.util.Collection)
+         */
+        public boolean addAll(Collection c) {
+            DefaultStateSet dss = (DefaultStateSet) c;
+            bits.or(dss.bits);
+            modcount++;
+            return true;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#containsAll(java.util.Collection)
+         */
+        public boolean containsAll(Collection c) {
+            DefaultStateSet dss = (DefaultStateSet) c;
+            BitSet bs = new BitSet();
+            bs.or(bits);
+            bs.and(dss.bits);
+            modcount++;
+            return bs.equals(dss.bits);
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#removeAll(java.util.Collection)
+         */
+        public boolean removeAll(Collection c) {
+            DefaultStateSet dss = (DefaultStateSet) c;
+            bits.andNot(dss.bits);
+            modcount++;
+            return true;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#retainAll(java.util.Collection)
+         */
+        public boolean retainAll(Collection c) {
+            DefaultStateSet dss = (DefaultStateSet) c;
+            bits.and(dss.bits);
+            modcount++;
+            return true;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#iterator()
+         */
+        public Iterator iterator() {
+      /* reset iterator */
+            bit = modcount = mods = 0;
+            return it;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Set#toArray(java.lang.Object[])
+         */
+        public Object[] toArray(Object[] a) {
+            Object[] ret;
+            if (a.length == size())
+                ret = a;
+            else { /* create array dynamically */
+                ret = (Object[]) Array.newInstance(a.getClass().getComponentType(),
+                        size());
+            }
+            Iterator it = iterator();
+            int i = 0;
+            while (it.hasNext()) {
+                DefaultState ds = (DefaultState) it.next();
+                ret[ds.i] = ds;
+            }
+            return ret;
+        }
+
+    }
+
+    // //////////////////////////////////////////////////////
+    // FIELDS
+    // /////////////////////////////////////////////////////
+
+    protected int id = 0;
+
+    Automaton automaton;
+
+    // //////////////////////////////////////////////////////
+    // PUBLIC METHODS
+    // /////////////////////////////////////////////////////
+
+    DefaultStateFactory(Automaton a) {
         this.automaton = a;
     }
 
@@ -124,8 +394,10 @@ public class DefaultStateFactory<L, Tr extends Transition<L>, T extends Builder<
      * Creates a new state which is initial and terminal or not, depending on the
      * value of parameters.
      *
-     * @param initial  if true, this state will be initial; otherwise this state will be non initial.
-     * @param terminal if true, this state will be terminal; otherwise this state will be non terminal.
+     * @param initial  if true, this state will be initial; otherwise this state will be
+     *                 non initial.
+     * @param terminal if true, this state will be terminal; otherwise this state will be
+     *                 non terminal.
      */
     public State create(boolean initial, boolean terminal) {
         return new DefaultState(id++, initial, terminal);
@@ -136,8 +408,8 @@ public class DefaultStateFactory<L, Tr extends Transition<L>, T extends Builder<
      * 
      * @see rationals.StateFactory#stateSet()
      */
-    public Set<State> stateSet() {
-    	return new HashSet<State>();
+    public Set stateSet() {
+        return new DefaultStateSet(this);
     }
 
     /*
@@ -145,22 +417,19 @@ public class DefaultStateFactory<L, Tr extends Transition<L>, T extends Builder<
      * 
      * @see rationals.StateFactory#stateSet(java.util.Set)
      */
-    @Override
-    public Set<State> stateSet(Set<State> s) {
-    	Set<State> result = new HashSet<>();
-    	result.addAll(s);
-        return s;
+    public Set stateSet(Set s) {
+        return new DefaultStateSet((DefaultStateSet) s, this);
     }
 
-    public DefaultStateFactory<L, Tr, T> clone() {
+    public Object clone() {
+        DefaultStateFactory cl;
         try {
-        	@SuppressWarnings("unchecked")
-			DefaultStateFactory<L, Tr, T> cl = (DefaultStateFactory<L, Tr, T>) super.clone();
-            cl.id = 0;
-            return cl;
-        } catch (CloneNotSupportedException ex) {
-            throw new Error(ex);
+            cl = (DefaultStateFactory) super.clone();
+        } catch (CloneNotSupportedException e) {
+            cl = null;
         }
+        cl.id = 0;
+        return cl;
     }
 
     /*
@@ -168,7 +437,7 @@ public class DefaultStateFactory<L, Tr extends Transition<L>, T extends Builder<
      * 
      * @see rationals.StateFactory#setAutomaton(rationals.Automaton)
      */
-    public void setAutomaton(Automaton<L, Tr, T> automaton) {
+    public void setAutomaton(Automaton automaton) {
         this.automaton = automaton;
     }
 
